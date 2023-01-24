1169ee52ab12a9dccd6951924f888071fa00e0a4
min park <minee.park@protonmail.com>
2023-01-24 22:30:15 +0900

    Live API Tests added

---
+++ tests/local_post.py
@@ -0,0 +1,33 @@
+import json
+import requests
+
+site = 'http://0.0.0.0:8000/predict'
+
+payload = {'age': 31,
+    'workclass': 'Private',
+    'fnlgt': 45781,
+    'education': 'Masters',
+    'education-num': 14,
+    'marital-status': 'Never-married',
+    'occupation': 'Prof-speciality',
+    'relationship': 'Not-in-family',
+    'race': 'White',
+    'sex': 'Female',
+    'capital-gain': 14000,
+    'capital-loss': 0,
+    'hours-per-week': 55,
+    'native-country': 'United-States',
+    'salary': '>50K'
+}
+
+headers = {'Content-Type':'application/json'}
+
+if __name__ == '__main__':
+    response = requests.post(site, data=json.dumps(payload), headers=headers)
+
+    if response.status_code == 200:
+        result = response.json()['result']
+        print(f'The prediction for the given data is: {result}')
+
+    else:
+        print(f'Bad request (response code: {response.status_code})')
