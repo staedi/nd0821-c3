7b5c3fa0425e6447dac77b272b7e0338c9ac8da0
min park <minee.park@protonmail.com>
2023-01-27 22:50:26 +0900

    if __name__ == '__main__'

--- train_model.py
+++ train_model.py
@@ -7,50 +7,51 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import pickle
 
-# Add code to load in the data.
-data = pd.read_csv('data/census.csv')
+if __name__ == '__main__':
+    # Add code to load in the data.
+    data = pd.read_csv('data/census.csv')
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+    # Optional enhancement, use K-fold cross validation instead of a train-test split.
+    train, test = train_test_split(data, test_size=0.20)
 
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
 
-# Proces the test data with the process_data function.
-X_test, y_test, encoder, lb = process_data(
-    test, categorical_features=cat_features, label=None, encoder=encoder, lb=lb, training=False
-)
+    # Proces the test data with the process_data function.
+    X_test, y_test, encoder, lb = process_data(
+        test, categorical_features=cat_features, label=None, encoder=encoder, lb=lb, training=False
+    )
 
-# Train and save a model.
-model = ml.model.train_model(X_train, y_train)
+    # Train and save a model.
+    model = ml.model.train_model(X_train, y_train)
 
-model_paths = {'model': 'model/model.pkl', 'encoder': 'model/encoder.pkl', 'lb': 'model/lb.pkl'}
-for path_key,path in model_paths.items():
-    with open(path,'wb') as f:
-        pickle.dump(eval(path_key),f)
+    model_paths = {'model': 'model/model.pkl', 'encoder': 'model/encoder.pkl', 'lb': 'model/lb.pkl'}
+    for path_key,path in model_paths.items():
+        with open(path,'wb') as f:
+            pickle.dump(eval(path_key),f)
 
-# Get metrics performances and save to output text files
-output_paths = {'overall': 'model/output.txt', 'slice': 'model/slice_output.txt'}
-for output_key, output_path in output_paths.items():
-    # Slice data performances
-    if output_key == 'slice':
-        metrics = ml.model.performance_on_slice(model, data=data, cat_features=cat_features, encoder=encoder, lb=lb, label='salary')
-    # Overall data performances
-    else:
-        metrics = ml.model.performance_overall(model, data=test, cat_features=cat_features, encoder=encoder, lb=lb, label='salary')
+    # Get metrics performances and save to output text files
+    output_paths = {'overall': 'model/output.txt', 'slice': 'model/slice_output.txt'}
+    for output_key, output_path in output_paths.items():
+        # Slice data performances
+        if output_key == 'slice':
+            metrics = ml.model.performance_on_slice(model, data=data, cat_features=cat_features, encoder=encoder, lb=lb, label='salary')
+        # Overall data performances
+        else:
+            metrics = ml.model.performance_overall(model, data=test, cat_features=cat_features, encoder=encoder, lb=lb, label='salary')
 
-    # Save to output text
-    with open(output_path,'w') as f:
-        metrics_str = metrics.to_string(index=False)
-        f.write(metrics_str)
+        # Save to output text
+        with open(output_path,'w') as f:
+            metrics_str = metrics.to_string(index=False)
+            f.write(metrics_str)
